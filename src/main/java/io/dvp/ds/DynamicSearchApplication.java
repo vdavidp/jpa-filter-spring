a886f2877f10c1081558195319497f26bf9e4794
david <pc.adicto@gmail.com>
2021-02-09 19:12:55 -0400

    Initial commit

---
+++ src/main/java/io/dvp/ds/DynamicSearchApplication.java
@@ -0,0 +1,13 @@
+package io.dvp.ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/dvp/ds/DynamicSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dvp.ds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
