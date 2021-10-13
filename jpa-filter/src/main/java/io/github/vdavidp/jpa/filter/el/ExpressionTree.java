5a5850c75098d489a4bf0e98fbcb22cb6518bed3
David <vp.david@hotmail.com>
2021-10-13 12:51:45 -0400

    Fixed usage of java 11 api

--- jpa-filter/src/main/java/io/github/vdavidp/jpa/filter/el/ExpressionTree.java
+++ jpa-filter/src/main/java/io/github/vdavidp/jpa/filter/el/ExpressionTree.java
@@ -64,7 +64,7 @@ public class ExpressionTree {
           .filter(Objects::nonNull)
           .min(Comparator.comparing(Token::getIndex));
     
-    if (tokenOp.isEmpty()) {
+    if (!tokenOp.isPresent()) {
       return null;
     }
     
